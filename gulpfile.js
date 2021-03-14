21c7be13f88d4a06cead8165aa16c78fb0712b69
Olha Kravchenko <olga19972010@yandex.by>
2021-03-14 19:37:57 +0300

    8.1 9.1 Made automation using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,48 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+const htmlmin = require("gulp-htmlmin");
+const minify = require("gulp-minify");
 
-// Styles
+const cleanBuildFolder = () => {
+  return del("build");
+};
 
-const styles = () => {
+const copyFontsAndImagesToBuild = () => {
+  return gulpfile.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+  ], {
+    base: "source"
+  })
+    .pipe(gulpfile.dest("build"));
+}
+
+const copyHtmlToBuild = () => {
+  return gulpfile.src([
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+    .pipe(gulpfile.dest("build"));
+}
+
+const copyCssToBuild = () => {
+  return gulpfile.src([
+    "source/css/**"
+  ], {
+    base: "source"
+  })
+    .pipe(gulpfile.dest("build"));
+}
+
+const makeCssFromSass = () => {
   return gulpfile.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
@@ -21,14 +59,76 @@ const styles = () => {
     .pipe(sync.stream());
 }
 
-exports.styles = styles;
+const minifyHtml = () => {
+  return gulpfile.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulpfile.dest("build"))
+    .pipe(sync.stream());
+}
 
-// Server
+const makeMinifiedCssFromSass = () => {
+  return gulpfile.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulpfile.dest("build/css"))
+    .pipe(sync.stream());
+}
+
+const minifyJs = () => {
+  return gulpfile.src("source/js/**/*.js")
+    .pipe(minify())
+    .pipe(gulpfile.dest("build/js"));
+}
+
+const makeSvgSprite = () => {
+  return gulpfile.src("build/img/**/sprite_*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulpfile.dest("build/img"))
+}
+
+const optimizeImages = () => {
+  return gulpfile.src("source/img/**/*.{ipg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulpfile.dest("build/img"))
+}
+
+const createWebp = () => {
+  return gulpfile.src("build/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulpfile.dest("build/img"))
+}
+
+const build = gulpfile.series(
+  cleanBuildFolder,
+  copyFontsAndImagesToBuild,
+  copyHtmlToBuild,
+  copyCssToBuild,
+  makeCssFromSass,
+  minifyHtml,
+  makeMinifiedCssFromSass,
+  minifyJs,
+  optimizeImages,
+  makeSvgSprite,
+  createWebp
+);
+exports.build = build;
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -36,16 +136,12 @@ const server = (done) => {
   });
   done();
 }
-
 exports.server = server;
 
-// Watcher
-
 const watcher = () => {
-  gulpfile.watch("source/sass/**/*.scss", gulpfile.series("styles"));
-  gulpfile.watch("source/*.html").on("change", sync.reload);
+  gulpfile.watch("source/sass/**/*.scss", gulpfile.series(makeCssFromSass, copyCssToBuild, makeMinifiedCssFromSass));
+  gulpfile.watch("source/js/**/*.js", gulpfile.series(minifyJs));
+  gulpfile.watch("source/*.html", gulpfile.series(minifyHtml));
+  gulpfile.watch("build/**/*.*").on("change", sync.reload);
 }
-
-exports.default = gulpfile.series(
-  styles, server, watcher
-);
+exports.default = gulpfile.series(build, server, watcher);
